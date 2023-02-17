fdac0295710e1ad2e884b9d3b149cfa8357d4d7b
FiRe <levih@n11.dev>
2023-02-17 01:34:40 -0600

    Forgot .gitignore
    Signed-off-by: FiRe <levih@n11.dev>

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fire/Link
-# Build directory: /home/fire/Link
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "/home/fire/Link/linktest")
-set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "/home/fire/Link/CMakeLists.txt;21;add_test;/home/fire/Link/CMakeLists.txt;0;")
