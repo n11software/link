f4172e8c2f7ff1c4bcb2fbc31bb0b239ddff660e
FiRe <levih@n11.dev>
2023-03-19 01:46:32 -0500

    Test: add proper testing
    Signed-off-by: FiRe <levih@n11.dev>

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,39 +2,76 @@
 #include <iostream>
 #include <fstream>
 #include <openssl/ssl.h>
+#include <thread>
 
+bool ready = false;
+bool ssl = false;
+
+void* client(void* arg) {
+    while (!ready) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    Link::Request* req = new Link::Request(std::string(ssl?"https":"http") + "://localhost:8080/");
+    Link::Client client(req);
+    Link::Response* res = client.Send();
+    if (res->GetStatus() == 200 && res->GetBody() == "Hello World!") {
+        std::cout << "\033[1;32mTest passed" << std::endl;
+    } else {
+        std::cout << "\033[1;31mTest failed" << std::endl;
+    }
+    ready = false;
+    return nullptr;
+}
+
+Link::Server server(8080);
 int main(int argc, char** argv) {
     if (SSLeay() < 0x30000000L) {
         std::cout << "OpenSSL version is lower than 3.0.0" << std::endl;
         return 0;
     }
-    bool server = false;
-    bool https = false;
-    if (argc > 1) {
-        if (std::string(argv[1]) == "server") server = true;
-        if (std::string(argv[1]) == "https") https = true;
-    }
-    if (server) {
-        Link::Server server(8080);
-        server.SetStaticPages("public/");
-        server.Get("/", [](Link::Request* request, Link::Response* response) {
-            if (request->GetParam("message") != "") response->SetBody(request->GetParam("message"));
-            else response->SetBody("Hello, world!");
-        });
-        server.Get("/:message", [](Link::Request* request, Link::Response* response) {
-            response->SetBody(request->GetParam("message"));
-        });
-        server.EnableSSL("certificate.pem", "key.pem");
-        server.EnableMultiThreading();
-        server.EnableDebugging();
-        server.SetStartMessage("Server started on port 8080");
-        server.Start();
-    } else {
-        Link::Request* request = new Link::Request(https?"https://localhost:8080/":"http://localhost:8080/");
-        Link::Client client(request);
-        Link::Response* response = client.Send();
-        std::cout << response->GetBody() << std::endl;
-    }
+
+    std::thread t;
+    t = std::thread(client, nullptr);
+    t.detach();
+    
+
+    server.Get("/", [](Link::Request* req, Link::Response* res) {
+        res->SetBody("Hello World!");
+        server.Stop();
+    });
+
+    ready = true;
+    server.Start();
+
+    server.EnableMultiThreading();
+
+    t = std::thread(client, nullptr);
+    t.detach();
+
+    ready = true;
+
+    server.Start();
+
+    ssl = true;
+
+    server.EnableSSL("certificate.pem", "key.pem");
+
+    t = std::thread(client, nullptr);
+    t.detach();
+
+    ready = true;
+
+    server.Start();
+
+    server.DisableMultiThreading();
+
+    t = std::thread(client, nullptr);
+    t.detach();
+
+    ready = true;
+
+    server.Start();
 
     return 0;
 }
